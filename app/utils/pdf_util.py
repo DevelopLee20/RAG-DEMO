07e78351813c295f7e9a3bb472d9bf6243e6fa33
leeing1414 <leeing1414@gmail.com>
2025-08-12 14:51:30 +0900

    PDF 파싱 코드 작성

---
+++ app/utils/pdf_util.py
@@ -0,0 +1,30 @@
+import os
+import PyPDF2
+from fastapi import UploadFile
+
+from app.utils.text_util import preprocessing_text
+
+def parse_pdf(file: str | UploadFile) -> list[str]:
+    """pdf 파일 또는 UploadFile 스트림을 받아서 파싱 리스트 반환
+
+    Args:
+        file (str): 파일명 또는 파일 스트림 객체
+
+    Returns:
+        list[str]: 파싱 리스트 반환
+    """
+    parse_list = []
+    
+    # file의 타입에 따라 형식 변경
+    if type(file) == str:
+        pdf = file
+    else:
+        pdf = file.file
+    
+    for page in PyPDF2.PdfReader(pdf).pages:
+        text = page.extract_text()
+        processed_text = preprocessing_text(text=text)
+        
+        parse_list.append(processed_text)
+    
+    return parse_list
---
+++ app/utils/text_util.py
@@ -0,0 +1,15 @@
+import re
+
+def preprocessing_text(text: str) -> str:
+    """텍스트 전처리 함수
+
+    Args:
+        text (str): 전처리할 텍스트
+
+    Returns:
+        str: 전처리된 텍스트
+    """
+    text = re.sub(r"[^ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9()+\-*/=<>%., ]", "", text)
+    text = re.sub(r" {2,}", " ", text)
+    
+    return text
