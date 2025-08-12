3296b2e720d4ac76edc7a271bec9a9d7e9a797aa
leeing1414 <leeing1414@gmail.com>
2025-08-12 14:59:30 +0900

    청크 리스트 반환 코드 작성

---
+++ app/utils/langchain_util.py
@@ -0,0 +1,29 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+
+splitter = None
+
+def get_splitter() -> RecursiveCharacterTextSplitter:
+    """텍스트 파싱 스플리터 객체 반환
+
+    Returns:
+        RecursiveCharacterTextSplitter: 스플리터 객체
+    """
+    if splitter is None:
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=20
+        )
+    
+    return splitter
+
+def get_chunks_to_text(texts: list[str]) -> list[Document]:
+    """텍스트를 받아서 청크 리스트를 반환하는 함수
+
+    Args:
+        texts (list[str]): 텍스트 리스트
+
+    Returns:
+        list[Document]: 청크 리스트 객체
+    """
+    return get_splitter().create_documents(texts=texts)
